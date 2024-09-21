5a8d695c6afc23d83bbb81c9d3f859fd82b7eb78
zzg <zzg.cn@hotmail.com>
2024-09-21 14:58:15 +0800

    重构(staffMain): 优化多标签页和菜单项数据流
    优化了staffMain组件中的多标签页和菜单项数据流处理。此更改重构了标签页的添加和移除逻辑，确保
    通过过滤函数直接更新editableTabs数组。此外，对菜单项的结构和样式进行了微调，以增强可读性和
    一致性。同时，处理了潜在的导航错误，避免了由于快速导航导致的冗余导航错误。
    
    还有一点重要的是，现在窗口大小调整事件处理器和主组件高度设置更加健壮，能够正确地响应窗口大小调整事件。

--- src/views/staff/staffMain.vue
+++ src/views/staff/staffMain.vue
@@ -2,33 +2,45 @@
   <div id="staffMain">
     <el-container>
       <el-header
-          style="background: linear-gradient(to right, #4A90E2, #4A90E2); color: white; padding: 0 20px; box-shadow: 0 2px 4px rgb(74, 144, 226);">
-        <div style="font-size: 1.5em; line-height: 60px;">好医生后台管理系统</div>
+        style="
+          background: linear-gradient(to right, #4a90e2, #4a90e2);
+          color: white;
+          padding: 0 20px;
+          box-shadow: 0 2px 4px rgb(74, 144, 226);
+        "
+      >
+        <div style="font-size: 1.5em; line-height: 60px">
+          好医生后台管理系统
+        </div>
       </el-header>
       <el-container>
-        <el-aside width="230px" style="background-color: #2c3e50;">
+        <el-aside width="230px" style="background-color: #2c3e50">
           <el-menu
-              class="el-menu-vertical-demo"
-              background-color="transparent"
-              text-color="#ffffff"
-              :unique-opened=true
-              active-text-color="#3498db"
-              :default-active="activeMenu"
-              @select="handleSelect"
-              :default-openeds="$router.path"
-              :router=true
+            class="el-menu-vertical-demo"
+            background-color="transparent"
+            text-color="#ffffff"
+            :unique-opened="true"
+            active-text-color="#3498db"
+            :default-active="activeMenu"
+            @select="handleSelect"
+            :default-openeds="$router.path"
+            :router="true"
           >
-            <el-submenu v-for="m in empMenu" :key="m.id" :index="m.id.toString()">
+            <el-submenu
+              v-for="m in empMenu"
+              :key="m.id"
+              :index="m.id.toString()"
+            >
               <template slot="title">
                 <i :class="m.icon"></i>
                 <span>{{ m.name }}</span>
               </template>
               <el-menu-item-group>
                 <el-menu-item
-                    v-for="s in m.son"
-                    :key="s.id"
-                    :index="s.url"
-                    @click="addTab(s)"
+                  v-for="s in m.son"
+                  :key="s.id"
+                  :index="s.url"
+                  @click="addTab(s)"
                 >
                   <i :class="s.icon"></i>
                   {{ s.name }}
@@ -37,14 +49,21 @@
             </el-submenu>
           </el-menu>
         </el-aside>
-        <el-main style="padding: 20px; background-color: #ffffff;">
-          <el-tabs v-model="editableTabsValue" type="card" closable @tab-remove="removeTab">
+        <el-main style="padding: 20px; background-color: #ffffff">
+          <el-tabs
+            v-model="editableTabsValue"
+            @tab-click="tabClick"
+            type="card"
+            closable
+            @tab-remove="removeTab"
+          >
             <el-tab-pane
-                v-for="item in editableTabs"
-                :key="item.id"
-                :label="item.name"
-                :name="item.id">
-              <router-view/>
+              v-for="item in editableTabs"
+              :key="item.url"
+              :label="item.name"
+              :name="item.id"
+            >
+              <router-view />
             </el-tab-pane>
           </el-tabs>
         </el-main>
@@ -53,7 +72,7 @@
   </div>
 </template>
 <script>
-import {getInfo} from "@/api/staff/staffMain";
+import { getInfo } from "@/api/staff/staffMain";
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
@@ -64,7 +83,7 @@ export default {
       editableTabsValue: "0",
       editableTabs: [],
       activeMenu: "", // 默认选中的菜单项
-      empMenu: []
+      empMenu: [],
     };
   },
   methods: {
@@ -72,40 +91,39 @@ export default {
       this.activeMenu = key;
     },
     addTab(targetName) {
-      let newTabName = targetName.id.toString();
+      let newTabName = targetName.url;
       let newTabLabel = targetName.name;
-      let routeName = targetName.url; // 假设您已经将子菜单的 url 属性设置为路由名称
+      let routeName = targetName.url;
 
-      // 检查是否已经存在该路由的标签页
-      const tabExists = this.editableTabs.some(tab => tab.routeName === routeName);
-
-      // 如果不存在，才添加新标签页
+      const tabExists = this.editableTabs.some(
+        (tab) => tab.routeName === routeName
+      );
       if (!tabExists) {
         this.editableTabs.push({
           id: newTabName,
           name: newTabLabel,
-          routeName: routeName
+          routeName: routeName,
         });
       }
-      // 检查目标路由是否已经是当前路由
-      if (this.$route.path !== routeName) {
-        // 如果不是当前路由，才进行跳转
-        this.$router.push(routeName).then(() => {
-          this.$nextTick(() => {
-            this.editableTabsValue = newTabName;
+      // Handle both Promise and non-Promise returns from $router.push
+      const navigationResult = this.$router.push(routeName);
+      if (navigationResult && typeof navigationResult.then === "function") {
+        navigationResult
+          .then(() => {
+            this.$nextTick(() => {
+              this.editableTabsValue = newTabName;
+            });
+          })
+          .catch((error) => {
+            if (error.name !== "NavigationDuplicated") throw error;
           });
-        }).catch(err => {
-          // 处理导航错误，例如 "Avoided redundant navigation" 错误
-          if (err.name !== 'NavigationDuplicated') throw err;
-        });
       } else {
-        // 如果已经是当前路由，则直接切换到该标签页
         this.editableTabsValue = newTabName;
       }
     },
     removeTab(targetName) {
       // 从 editableTabs 数组中移除指定 id 的标签页
-      let tabs = this.editableTabs.filter(tab => tab.id !== targetName);
+      let tabs = this.editableTabs.filter((tab) => tab.id !== targetName);
       this.editableTabs = tabs;
 
       // 如果关闭的是当前标签页，则跳转到最后一个标签页或首页
@@ -116,22 +134,30 @@ export default {
         }
       }
     },
+    tabClick() {
+      this.$router.push({ path: this.editableTabsValue });
+    },
     setMainHeight() {
       const mainHeight = window.innerHeight - 60;
-      this.$el.querySelector('.el-main').style.height = `${mainHeight}px`;
-    }
+      this.$el.querySelector(".el-main").style.height = `${mainHeight}px`;
+    },
   },
   beforeDestroy() {
-    window.removeEventListener('resize', this.setMainHeight);
+    window.removeEventListener("resize", this.setMainHeight);
   },
   mounted() {
     this.setMainHeight();
-    window.addEventListener('resize', this.setMainHeight);
-    getInfo().then(response => {
-      this.info = response.data;
-      this.empMenu = response.data.empMenu;
+    window.addEventListener("resize", this.setMainHeight);
+    getInfo().then((response) => {
+      if (response.code === 200) {
+        this.info = response.data;
+        this.empMenu = response.data.empMenu;
+      } else {
+        this.$message.error(response.msg);
+        this.$router.push({ name: "Login" });
+      }
     });
-  }
+  },
 };
 </script>
 <style>
@@ -147,7 +173,6 @@ export default {
 
 .el-container {
   height: 100%;
-
 }
 
 .el-main {
